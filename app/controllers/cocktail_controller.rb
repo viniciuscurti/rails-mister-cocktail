c93ea0a79b8344f6ba1dc8ed799a6403acdb5dea
vinicius curti <vinicius_c_l@hotmail.com>
2020-08-20 19:44:35 -0300

    incomplete

---
+++ app/controllers/cocktail_controller.rb
@@ -0,0 +1,53 @@
+class CocktailController < ApplicationController
+    # FILTER (Execute set_cocktail method)
+    before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+    # READ ALL (GET) /cocktails
+    def index
+      @cocktails = Cocktail.all
+    end
+  
+    # READ ONE (GET) /cocktails/:id
+    def show
+    end
+  
+    # CREATE (GET) /cocktails/new
+    def new
+      @cocktail = Cocktail.new
+    end
+  
+    # CREATE (POST) /cocktails
+    def create
+      @cocktail = Cocktail.new(cocktail_params)
+      @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    end
+  
+    # EDIT (GET) /cocktails/:id/edit
+    def edit
+    end
+  
+    # EDIT (PATCH) /cocktails/:id
+    def update
+      @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    end
+  
+    # DESTROY (DELETE) /cocktails/:id
+    def destroy
+      @cocktail.destroy
+      redirect_to @cocktails_path
+    end
+  
+    private
+  
+    # STRONG PARAMS
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
+  
+    # BEFORE ACTION FILTER
+    def set_cocktail
+      @cocktail = cocktail.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[index new create show]
+  end
+  resources :doses, only: %i[destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

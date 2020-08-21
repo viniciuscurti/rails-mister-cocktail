cf7082bcb77a32da334158df4d5d5b77fb243063
vinicius curti <vinicius_c_l@hotmail.com>
2020-08-21 18:19:15 -0300

    First push

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: %i[index show new create] do
-    resources :doses, only: %i[index new create show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
   end
-  resources :doses, only: %i[destroy]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'cocktails#index'
+  resources :doses, only: [:destroy]
 end
